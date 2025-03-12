26585da027c8c95043ea98c3bdbd3332b923ad70
ganeshchinimilli <ganeshchinimilli4@gmail.com>
2025-03-12 05:44:35 +0530

    changes in learning

---
+++ GFG/3-12/first_prograam.py
@@ -0,0 +1,7 @@
+print('Hello world')
+a='tesft'
+def first():
+    a='test'
+    print(a)
+first()
+print(a)
---
+++ GFG/common_learninig.py
@@ -0,0 +1,15 @@
+# Indentation in python 
+# python variables
+    # no need to declare the variable types var,const,
+    # cannot start with digit .
+    # case sensitive (myvar and MyVar both are different)
+    # x,y,z=1,2,3
+    # basic casting functionlities are int(),float(),str()
+    # to get the type fo variable type()
+    # to use the global variable inside funciton use global syntax
+
+# Keywords in python are(to get the keywords use import keyword and do keyword.kwlist)
+    # False,await,else,import,pass,None,break,except,in,raise,True,class,finally,is,return,and,continue,for,lambda,try,as,def,from,nonlocal,while,assert,del,global,not,with,async,elif,if,or,yield
+    # lambda is an anonomys function
+# to Take input from user 
+    # input()
